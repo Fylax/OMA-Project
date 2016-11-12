e8fa18fc70dc16c3847e6ac0b9d0044f22d112d3
Francesco Vergona <vergonaf@gmail.com>
2016-11-12 10:57:37 +0100

    Aggiunta directory Francesco

--- OMA Project/OMA Project/Program.cs
+++ OMA Project/OMA Project/Program.cs
@@ -10,7 +10,9 @@ namespace OMA_Project
     {
         static void Main(string[] args)
         {
-            Problem x = new Problem(@"C:\Users\Fylax\Desktop\Material_assignment\input\Co_300_20_NT_0.txt");
+            //Problem x = new Problem(@"C:\Users\Fylax\Desktop\Material_assignment\input\Co_300_20_NT_0.txt");
+            Problem x = new Problem(@"C: \Users\vergo\Google Drive\PoliTO - Magistrale\• 1.1 Optimization Methods and Algorithms\Assignement\Materiale\material_assignment_v2\Material_assignment\input\Co_300_20\Co_300_20_T_19.txt");
+            
             int y = 0;
         }
     }
