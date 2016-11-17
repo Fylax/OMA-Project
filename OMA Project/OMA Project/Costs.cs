29be71ced1fe7511b1ae72f06c872403b2c6d26f
Nico Caprioli <nico.caprioli@gmail.com>
2016-11-17 16:50:39 +0100

    min fun

--- OMA Project/OMA Project/Costs.cs
+++ OMA Project/OMA Project/Costs.cs
@@ -1,55 +1,47 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace OMA_Project
 {
     public class Costs
     {
-        private readonly int[][][][] matrix;
+        private readonly Dictionary<CostIndex, int[][]> matrix;
 
         public Costs(int timeSlots, int userTypes)
         {
-            matrix = new int[timeSlots][][][];
-            for (int i = 0; i < timeSlots; ++i)
-            {
-                matrix[i] = new int[userTypes][][];
-            }
+            matrix = new Dictionary<CostIndex, int[][]>(timeSlots * userTypes);
         }
 
         public void AddMatrix(int timeSlot, int userType, int[][] matrix)
         {
-            this.matrix[timeSlot][userType] = matrix;
+            this.matrix.Add(new CostIndex(timeSlot, userType), matrix);
         }
 
-        public int GetCost(int timeslot, int userType, int start, int destination) => matrix[timeslot][userType][start][destination];
+        public int GetCost(int timeSlot, int userType, int start, int destination) => matrix[new CostIndex(timeSlot, userType)][start][destination];
 
-        public void GetMin(int destination, int[] taskPerUser)
+        public System.Tuple<CostIndex, int> GetMin(int destination, int[] taskPerUser)
         {
             int maxTask = taskPerUser.Max();
             int minStart = 0;
-            int minUserType = 0;
-            int minTimeSlot = 0;
+            CostIndex minUserTime = new CostIndex();
             int min = int.MaxValue;
-            for (int t = 0; t < matrix.Length; ++t)
+            foreach (var relativeCosts in matrix)
             {
-                for (int u = 0; u < matrix[t].Length; ++u)
+                for (int i = 0; i < relativeCosts.Value.Length; ++i)
                 {
-                    for (int i = 0; i < matrix[t][u].Length; ++i)
+                    if (i != destination)
                     {
-                        if (i != destination)
+                        int cost = relativeCosts.Value[i][destination] * maxTask / taskPerUser[relativeCosts.Key.UserType];
+                        if (cost < min)
                         {
-                            int cost = matrix[t][u][i][destination] * maxTask / taskPerUser[u];
-                            if (cost < min)
-                            {
-                                min = cost;
-                                minStart = i;
-                                minUserType = u;
-                                minTimeSlot = t;
-                            }
+                            min = cost;
+                            minStart = i;
+                            minUserTime = relativeCosts.Key;
                         }
                     }
                 }
             }
+            return System.Tuple.Create(minUserTime, minStart);
         }
     }
 }
--- OMA Project/OMA Project/Program.cs
+++ OMA Project/OMA Project/Program.cs
@@ -11,13 +11,15 @@ namespace OMA_Project
     {
         static void Main(string[] args)
         {
-            Problem x = new Problem(@"C:\Users\Fylax\Desktop\Material_assignment\input\Co_30_20_NT_0.txt");
-            x.Matrix.GetMin(13, x.TaskPerUser);
+            Problem x = new Problem(@"C:\Users\Fylax\Desktop\Material_assignment\input\Co_30_1_NT_0.txt");
             var z = Stopwatch.StartNew();
-            x.GreedySolution();
+            var h = x.Matrix.GetMin(1, x.TaskPerUser);
             var q = z.ElapsedMilliseconds;
+            x.GreedySolution();
+
             //Problem x = new Problem(@"C: \Users\vergo\Google Drive\PoliTO - Magistrale\• 1.1 Optimization Methods and Algorithms\Assignement\Materiale\material_assignment_v2\Material_assignment\input\Co_300_20\Co_300_20_T_19.txt");            
-            int y = 0;
+            Console.WriteLine(q);
+            Console.Read();
         }
     }
 }
